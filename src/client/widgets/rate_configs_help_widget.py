4bcb15e800e0267594b615c0880eeaf1600b8195
Benjamin Abendroth <braph93@gmx.de>
2026-03-21 11:29:36 +0100

    Rate Configs Help: Make it tabbed; add usage text

--- src/client/widgets/rate_configs_help_widget.py
+++ src/client/widgets/rate_configs_help_widget.py
@@ -1,8 +1,14 @@
-RATE_CONFIG_HELP_TEXT = """\
-The rating feature analyzes whether a configuration can be executed safely on the current system. A positive rating does not necessarily mean that the configuration will work correctly, only that it appears reasonable and non-destructive.<br />
+RATE_CONFIG_USAGE_TEXT = """\
+The rating feature analyzes whether a configuration can be applied safely on the current system. A positive rating does not necessarily mean that the configuration will work correctly, only that it appears reasonable and non-destructive.<br />
 <br />
 Configurations that reference the same registers and ACPI methods are grouped together.<br />
 <br />
+It is recommended to start by testing <b>one configuration from each group</b>. Within a group, <b>try to choose a configuration whose name closely matches your laptop model</b>, as it is more likely to work correctly.<br />
+<br />
+If a configuration only works partially—for example, if the fan does not fully stop or does not reach its maximum speed—you can try other configurations from the same group.\
+"""
+
+RATE_CONFIG_INTERPRETING_RESULTS_TEXT = """\
 <b>FULL MATCH</b><br />
 Indicates that the register is a known fan register.<br />
 <br />
@@ -25,7 +31,20 @@ For fan registers, at least a <b>MINIMAL MATCH</b> is required to consider a con
 <br />
 For RegisterWriteConfiguration registers, some registers may not yet be present in the rule database. In these cases, a <b>NO MATCH</b> result may still be acceptable.<br />
 <br />
-If in doubt, it is recommended to dump the firmware using <b>sudo nbfc acpi-dump dsl</b> and manually analyze the registers used by the configuration file. This requires some technical knowledge."""
+If in doubt, it is recommended to dump the firmware using <b>sudo nbfc acpi-dump dsl</b> and manually analyze the registers used by the configuration file. This requires some technical knowledge.\
+"""
+
+class RateConfigsHelpTextWidget(QScrollArea):
+    def __init__(self, markup):
+        super().__init__()
+
+        self.setWidgetResizable(True)
+
+        label = QLabel(markup)
+        label.setWordWrap(True)
+        label.setStyleSheet("padding: 10px")
+
+        self.setWidget(label)
 
 class RateConfigsHelpWidget(QWidget):
     def __init__(self):
@@ -37,17 +56,14 @@ class RateConfigsHelpWidget(QWidget):
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-        scroll_area = QScrollArea()
-        scroll_area.setWidgetResizable(True)
-        layout.addWidget(scroll_area)
+        self.usage = RateConfigsHelpTextWidget(RATE_CONFIG_USAGE_TEXT)
+        self.interpreting = RateConfigsHelpTextWidget(RATE_CONFIG_INTERPRETING_RESULTS_TEXT)
 
-        label = QLabel(RATE_CONFIG_HELP_TEXT)
-        label.setWordWrap(True)
-        scroll_area.setWidget(label)
+        self.tab_widget = QTabWidget()
+        self.tab_widget.addTab(self.usage, "Usage")
+        self.tab_widget.addTab(self.interpreting, "Interpreting Results")
+        layout.addWidget(self.tab_widget)
 
         button = QPushButton("Close")
-        button.clicked.connect(self.close_clicked)
+        button.clicked.connect(lambda: self.close())
         layout.addWidget(button)
-
-    def close_clicked(self):
-        self.close()
